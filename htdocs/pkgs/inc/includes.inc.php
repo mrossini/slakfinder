de6bd9f40f4aaf4b7184e159ed4691e8b8e6acee
Matteo Rossini <matteo@frattocchie.it>
2010-02-16 15:48:50 +0100

    Basi

---
+++ htdocs/pkgs/inc/includes.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+
+include 'config.inc.php';
+include 'libs/database.inc.php';
+
+?>
---
+++ htdocs/pkgs/index.php
@@ -0,0 +1,13 @@
+<?php
+include 'inc/includes.inc.php';
+
+echo "<html><head><title>test</title></head><body>\n";
+
+
+
+
+
+echo "</body></html>\n";
+
+
+?>
