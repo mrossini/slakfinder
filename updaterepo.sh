94303f89280998f41e05059153bfbaf769288998
Your Name <you@example.com>
2011-07-24 01:12:23 +0200

    add DIE on updaterepo

--- updaterepo.sh
+++ updaterepo.sh
@@ -5,8 +5,8 @@ echo "==============="
 date
 git pull
 cd $CWD/htdocs/
-REDEFINE= php update.php
-php update.php
+DIE= REDEFINE= php update.php
+DIE= php update.php
 #cd $CWD/htdocs/dev/
 #REDEFINE= php update.php
 #php update.php
