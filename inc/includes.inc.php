3810bfbf3a556dd90f9359d232cf7b56664b961c
Matteo Rossini <zerouno@slacky.it>
2012-08-31 14:59:45 +0200

    removed unused xml library

--- inc/includes.inc.php
+++ inc/includes.inc.php
@@ -15,7 +15,6 @@ include_once 'libs/internet.inc.php';
 include_once 'libs/repository.inc.php';
 include_once 'libs/package.inc.php';
 include_once 'libs/filelist.inc.php';
-include_once 'libs/xml.inc.php';
 include_once 'libs/history.inc.php';
 include_once 'libs/guestbook.inc.php';
 include_once 'libs/stats.inc.php';
